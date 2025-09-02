ed6ef47c10ed6d3ed8e065b71d10d2ace5dc92e4
Deslynn Fenyes <ST10251981@vcconnect.edu.za>
2025-09-02 08:47:30 +0200

    Welcome page completed

--- app/src/main/java/com/example/deflate/LoginActivity.kt
+++ app/src/main/java/com/example/deflate/LoginActivity.kt
@@ -1,29 +1,20 @@
 package com.example.deflate
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login_layout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        // Set up button click listener
-        val letsBeginButton = findViewById<TextView>(R.id.lets_begin_button)
-        letsBeginButton.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
         }
     }
 }
